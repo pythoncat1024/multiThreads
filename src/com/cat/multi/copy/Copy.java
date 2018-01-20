0575e93cace56e48b592fc46c97e31910dc580cb
pythonCat <pythoncat@qq.com>
2018-01-21 01:09:31 +0800

    [feat]:多线程复制文件功能完成(每个线程复制文件的一部分，需要使用到RandomAccessFile) 备注：此功能不需要考虑线程安全的问题

--- src/com/cat/multi/copy/Copy.java
+++ src/com/cat/multi/copy/Copy.java
@@ -1,14 +1,21 @@
 package com.cat.multi.copy;
 
-import javax.xml.transform.Source;
-import java.io.*;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
-import java.util.logging.Level;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
+ * <pre>
+ *
  * Created by cat on 2018/1/20.
  * 小应用：多线程拷贝数据
+ * 分析一下：多线程拷贝是否需要考虑线程安全？
+ *      1. 是否是多线程运行环境？ ---> 是
+ *      2. 是否需要访问共享数据？ ---> 否
+ *          * 为什么说没有访问共享数据，因为RandomAccessFile 通过 seek(pos)方法，分别访问文件的不同位置，不涉及共享啊！
+ *      3. 操作共享数据是否是多条语句？--> 是
+ *
+ * </pre>
  */
 public class Copy implements Runnable {
 
@@ -37,7 +44,7 @@ public class Copy implements Runnable {
      *
      * @throws IOException io
      */
-    public void copy() throws IOException {
+    private void copy() throws IOException {
 
         RandomAccessFile bin = new RandomAccessFile(srcPath, "r");
 
@@ -60,17 +67,18 @@ public class Copy implements Runnable {
      *
      * @throws IOException
      */
-    public void copy1() throws IOException {
+    public void copy1() throws IOException, InterruptedException {
         if (this.pos == this.end) {
             System.out.println(" 文件已经复制结束了....");
             return;
         }
+        Thread.sleep(100);
         RandomAccessFile bin = new RandomAccessFile(srcPath, "r");
         RandomAccessFile bout = new RandomAccessFile(dest, "rw");
 
         byte[] buffer = new byte[bufferSize];
         int read;
-        long total = pos;
+        long total = 0;
         bin.seek(pos);
         bout.seek(pos);
         while ((read = bin.read(buffer)) != -1) {
@@ -78,41 +86,14 @@ public class Copy implements Runnable {
             bout.write(buffer, 0, read);
             total += read;
 
-            if (total == this.end) {
-                System.out.println("我的任务完成了. 当前完成的数据为：" + total);
+            if (total + pos == this.end) {
+                System.out.println(Thread.currentThread().getName()+" 我的任务完成了. 当前完成的数据为：" + total);
                 System.out.println("c1. from：" + pos + " , end=" + end + " , total=" + new File(srcPath).length());
                 this.pos = this.end;
                 break;
             } else if (total > this.end) {
                 throw new RuntimeException("end 计算出错了，需要修改程序，否则数据复制会出错...");
             }
-        }
-//        System.out.println("---------------------------total=" + total);
-
-        bout.close();
-        bin.close();
-    }
-
-    /**
-     * need copy 1
-     *
-     * @throws IOException
-     */
-    public void copy2() throws IOException {
-
-        RandomAccessFile bin = new RandomAccessFile(srcPath, "r");
-
-        RandomAccessFile bout = new RandomAccessFile(dest, "rw");
-//        BufferedInputStream bin = new BufferedInputStream(new FileInputStream(src));
-//        BufferedOutputStream bout = new BufferedOutputStream(new FileOutputStream(df));
-
-        byte[] buffer = new byte[1024];
-        int read;
-        System.out.println("w seek2=" + bout.length());
-        bout.seek(bout.length());
-        bin.seek(bout.length());
-        while ((read = bin.read(buffer)) != -1) {
-            bout.write(buffer, 0, read);
         }
         bout.close();
         bin.close();
@@ -121,5 +102,16 @@ public class Copy implements Runnable {
     @Override
     public void run() {
 
+        while (true) {
+            if (this.pos == this.end) {
+                System.out.println(" 文件已经复制结束了....");
+                break;
+            }
+            try {
+                copy1();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
