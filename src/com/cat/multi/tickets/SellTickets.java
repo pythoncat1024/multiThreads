0fc40674a69526dd7e8b9b2fb75f899ce5db11e0
pythonCat <pythoncat@qq.com>
2018-01-20 20:05:16 +0800

    [feat]:添加注释说明：为什么加了同步锁就可以让卖票程序数据正常

--- src/com/cat/multi/tickets/SellTickets.java
+++ src/com/cat/multi/tickets/SellTickets.java
@@ -16,16 +16,25 @@ public class SellTickets implements Runnable {
     @Override
     public void run() {
         while (true) {
-
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            if (this.tickets > 0) {
-                System.out.println(Thread.currentThread().getName() + " 当前正在出售第 " + tickets + " 张票.");
-                tickets -= 1;
-            }
+            sell();
+        }
+    }
+
+    /**
+     * 为何该方法需要被同步锁保护？
+     * 因为改方法里面涉及到对共享数据的修改(this.tickets)
+     * 而且当前是多线程的环境，如果不保护，就不能让该方法被原子性的访问
+     * 原子性的访问(线程xx访问的同时，其他线程只能等待，不能同时访问该方法)
+     */
+    private synchronized void sell() {
+        if (this.tickets > 0) {
+            System.out.println(Thread.currentThread().getName() + " 当前正在出售第 " + tickets + " 张票.");
+            tickets -= 1;
         }
     }
 }
